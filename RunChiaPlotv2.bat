e6850d24aeb8e35f4ade070664800ba9092cbd49
Umfriend <83405736+Umfriend@users.noreply.github.com>
2021-05-02 19:21:41 +0200

    Add files via upload
    Bug fix: After 10 plots, (in a cylce of 10) the cyclical numbering, so restart with _L01, did not work.
    Update: After going through a cycle, the NTFS attribute Date created would not update when a new log for that cycle_item(?) was made (So _L01 would now log a new / the 11th plot but the Date created would still be set at the date of the 1st plot). Now we check&delete if _L01 exists.
    Update: In case you want to edit the batch file dynamically and need to add code prior to the call of chia.exe, you can. Just delete the number of charachters you added from the star-line (not sure about line-feeds, I assume 1 char).

---
+++ RunChiaPlotv2.bat
@@ -0,0 +1,44 @@
+CD /d %LOCALAPPDATA%\chia-blockchain\app-1.1.1\resources\app.asar.unpacked\daemon\
+REM ECHO OFF
+REM PR = Parallel Run {1, 2, 3, etc)
+REM TO = Timeout before start (of first run) for staggering
+REM T1 = Tempfolder 1, T2 = Tempfolder 2, FF = Finalfolder
+REM LF = LogFolder, LC = LogCycle (How many plots do you want to retain logs for), LI = Number of LogCycle
+REM A following line is to have additional bytes in the batchfile prior to doing anything.
+REM You can in fact alter the code while running the batch files. Once it is done with a plot, it'll read the next
+REM command in the newly saved RunChiaPlot.bat **provided that** the next command (or any valid command) is still at the same byte-position
+REM So if code added before the call of chia.exe (or if you cange that call and the length is altered)
+REM use the star-line to adjust: Delete/add then number of characters you have added in code. Not sure about newline, I assume counts as 1 char.
+REM ************************************************************************************************************************
+SET PR=%1
+SET /A TO=%2 
+SET T1=%3
+SET T2=%4
+SET FF=%5
+SET LF=%6
+SET /A LC=%7
+SET /A LI=0
+ECHO Gonna wait for %TO%
+TIMEOUT /t %TO% >NUL
+DEL %T1%\*.* /F /Q
+DEL %T2%\*.* /F /Q
+:Run
+CLS
+SET /A LI=LI+1
+IF %LI% GTR %LC% (SET /A LI=1)
+SET SLI=%LI%
+IF %LI% LSS 10 (SET SLI=0%LI%)
+SET SPR=PR
+IF %PR% LSS 10 (SET SPR=0%PR%)
+SET LFN=%LF%\Plotter_%SPR%_L%SLI%.log
+REM I delete the existing log file so that the NTFS attribute Date created is updated. Easier to determine start and end time (or in my case, date ROFL).
+IF EXIST*%LFN% (DEL %LFN%)
+chia.exe plots create -k32 -b4500 -r2 -t%T1% -2%T2% -d%FF% >%LFN%
+if exist "c:\chiatest\stop.txt" (
+	goto :end
+) else (
+goto :Run
+)
+:End
+ECHO Stop.txt was found, I ended.
+Exit
